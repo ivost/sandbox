dabea1fb7bf83bdf5361acab1ee77acc21eca202
Ivo Stoyanov <ivostoy@gmail.com>
2020-02-08 12:36:55 -0800

    start

--- java/reactor/src/test/java/com/example/reactor/ReactorApplicationTests.java
+++ java/reactor/src/test/java/com/example/reactor/ReactorApplicationTests.java
@@ -2,12 +2,26 @@ package com.example.reactor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ReactorApplicationTests {
 
 	@Test
 	void contextLoads() {
+		List<Integer> elements = new ArrayList<>();
+
+		Flux.just(1, 2, 3, 4)
+				.log()
+				.subscribe(elements::add);
+
+		assertThat(elements).containsExactly(1, 2, 3, 4);
+
 	}
 
 }

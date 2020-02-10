0c1965dc16173662621e6da0c3271533f6e96ca0
Ivo Stoyanov <ivostoy@gmail.com>
2020-02-09 17:26:04 -0800

    wip

---
+++ java/spring-reactive-master/reactive-producer-consumer/reactive-web-consumer/src/main/java/com/mynotes/spring/reactive/reactivewebapp/calls/CallPersonUsingWebClient_Step4.java
@@ -0,0 +1,33 @@
+package com.mynotes.spring.reactive.reactivewebapp.calls;
+
+import java.time.Duration;
+import java.time.Instant;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.mynotes.spring.reactive.reactivewebapp.Person;
+
+public class CallPersonUsingWebClient_Step4 {
+
+	private static final Logger logger = LoggerFactory.getLogger(CallPersonUsingWebClient_Step4.class);
+
+	private static WebClient client = WebClient.create("http://localhost:8080");
+
+	public static void main(String[] args) {
+
+		Instant start = Instant.now();
+
+		for (int i = 1; i <= 5; i++) {
+			client.get().uri("/person/{id}", i).retrieve().bodyToMono(Person.class).block();
+		}
+
+		logTime(start);
+	}
+
+	private static void logTime(Instant start) {
+		logger.debug("Elapsed time: " + Duration.between(start, Instant.now()).toMillis() + "ms");
+	}
+
+}
